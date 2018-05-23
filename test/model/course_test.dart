6be9c7da12c09292d8676cd77420475c8b72f7e3
EduardBartolovic <zxeduardxz@gmail.com>
2018-05-23 16:03:42 +0200

    Added tests for welcome page
    added timeouts to older tests

--- test/model/course_test.dart
+++ test/model/course_test.dart
@@ -1,6 +1,7 @@
 import 'package:cie_team1/model/course/course.dart';
 import 'package:test/test.dart';
 
+@Timeout(const Duration(seconds: 10))
 
 void main(){
 
---
+++ test/views/start_test.dart
@@ -0,0 +1,43 @@
+import 'package:cie_team1/views/start.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
+@Timeout(const Duration(seconds: 20))
+void main() {
+
+  testWidgets('1 widgetTest', (WidgetTester tester) async {
+    // Tells the tester to build a UI based on the widget tree passed to it
+    await tester.pumpWidget(
+      new StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return new MaterialApp(
+            home: new Material(
+              child: new Center(
+                  child: new WelcomePage()
+              ),
+            ),
+          );
+        },
+      ),
+    );
+
+
+    final Iterable<Widget> listOfWidgets = tester.allWidgets;
+
+    int counter = 0;
+    for (Widget widget in listOfWidgets) {
+      if (widget is Text) {
+        if (counter == 0) {
+          expect(widget.data, 'Courses in English');
+          counter++;
+        } else if (counter == 1) {
+          expect(widget.data, 'Start');
+          counter++;
+        }
+      }
+    }
+
+  });
+
+}
+
