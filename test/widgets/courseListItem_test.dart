1319c9e25876d9f61c413dde3b3e42efce2a5c38
poroma <phum221238@gmail.com>
2018-05-24 23:36:30 +0200

    test widget(courseList)

--- test/widgets/courseListItem_test.dart
+++ test/widgets/courseListItem_test.dart
@@ -5,34 +5,6 @@ import 'package:cie_team1/widgets/courseList.dart';
 
 
 void main() {
-/*
-  testWidgets('1 widgetTest', (WidgetTester tester) async {
 
-    // You can use keys to locate the widget you need to test
-    var sliderKey = new UniqueKey();
 
-    // Tells the tester to build a UI based on the widget tree passed to it
-    await tester.pumpWidget(
-      new StatefulBuilder(
-        builder: (BuildContext context, StateSetter setState) {
-          return new MaterialApp(
-            home: new Material(
-              child: new Center(
-                  child: new CourseList(new CourseListPresenter())
-              ),
-            ),
-          );
-        },
-      ),
-    );
-
-    await tester.
-    //expect(find., equals(0.0));
-
-    // Taps on the widget found by key
-    await tester.tap(find.byKey(sliderKey));
-
-  });
-
-*/
 }
