433a17557e72f668aa951d2c0ad4043ca02c6de2
Sean Wallace <sean@Seans-MacBook-Pro.local>
2018-05-27 17:38:32 +0200

    Wrote tests for genericIcons and genericBorders

--- lib/generic/genericIcon.dart
+++ lib/generic/genericIcon.dart
@@ -7,6 +7,8 @@ class GenericIcon {
   static const int memStorageAvailable = 0xe86c;
   static const int memStoragePending = 0xe8fd;
   static const int memStorageUnavailable = 0xe888;
+  static const int memStorageActiveFavorite = 0xe87d;
+  static const int memStorageInactiveFavorite = 0xe87e;
   static const String fontFamily = 'MaterialIcons';
   static Icon buildGenericAvailabilityIcon(int availability) {
     IconData iconData;
@@ -33,10 +35,10 @@ class GenericIcon {
 
   static Icon buildGenericFavoriteIcon(bool isActive) {
     return new Icon(
-      (isActive ? const IconData(0xe87d, fontFamily: 'MaterialIcons'):
-        const IconData(0xe87e, fontFamily: 'MaterialIcons')),
+      (isActive ? const IconData(memStorageActiveFavorite, fontFamily: 'MaterialIcons'):
+        const IconData(memStorageInactiveFavorite, fontFamily: 'MaterialIcons')),
       size: CiEStyle.getCoursesListIconSize(),
-      color: const Color.fromRGBO(235, 87, 87, 1.0),
+      color: CiEColor.red,
     );
   }
 }
