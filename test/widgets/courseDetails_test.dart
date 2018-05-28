740ecf10bfcb841038eab8b26a37e7ac35563cb8
EduardBartolovic <zxeduardxz@gmail.com>
2018-05-28 15:25:06 +0200

    tests for course Detail page

---
+++ test/widgets/courseDetails_test.dart
@@ -0,0 +1,77 @@
+import 'package:cie_team1/widgets/courseDetails.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
+@Timeout(const Duration(seconds: 20))
+void main() {
+
+
+
+  group('settingspagewidget', ()
+  {
+    testWidgets('1 widgetTest for settings', (WidgetTester tester) async {
+// Tells the tester to build a UI based on the widget tree passed to it
+      await tester.pumpWidget(
+        new StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return new MaterialApp(
+              home: new Material(
+                child: new Center(
+                    child: new CourseDetails()
+                ),
+              ),
+            );
+          },
+        ),
+      );
+
+      final Iterable<Widget> listOfWidgets = tester.allWidgets;
+
+      int counter = 0;
+      for (Widget widget in listOfWidgets) {
+        if (widget is Text) {
+          if (counter == 0) {
+            expect(widget.data, 'Course Details');
+            counter++;
+          } else if (counter == 1) {
+            expect(widget.data, 'FK 07');
+            counter++;
+          } else if (counter == 2) {
+            expect(widget.data, 'Description');
+            counter++;
+          } else if (counter == 3) {
+            expect(widget.data.isEmpty, false);
+            counter++;
+          }else if (counter == 4) {
+            expect(widget.data, "Hours per Week:");
+            counter++;
+          }else if (counter == 5) {
+            expect(widget.data.isEmpty,false);
+            counter++;
+          }else if (counter == 6) {
+            expect(widget.data, 'ECTS:');
+            counter++;
+          }else if (counter == 7) {
+            expect(widget.data.isEmpty,false);
+            counter++;
+          }else if (counter == 8) {
+            expect(widget.data, "Available");
+            counter++;
+          }else if (counter == 9) {
+            expect(widget.data,"Contact");
+            counter++;
+          }
+
+        }
+      }
+    });
+
+
+
+
+
+  });
+
+
+
+}
