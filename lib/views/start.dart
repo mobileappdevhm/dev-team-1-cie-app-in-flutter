93286c3d70bbf6cee18f3f447178601534715a5f
Justine Probst <justine.probst@munichmotorsport.de>
2018-05-29 13:45:00 +0200

    removed uneccessary files and changed start button

--- lib/views/start.dart
+++ lib/views/start.dart
@@ -1,3 +1,5 @@
+import 'package:cie_team1/utils/cieColor.dart';
+import 'package:cie_team1/utils/cieStyle.dart';
 import 'package:cie_team1/utils/routes.dart';
 import 'package:cie_team1/utils/staticVariables.dart';
 import 'package:flutter/material.dart';
@@ -15,14 +17,15 @@ class WelcomePage extends StatelessWidget {
           ),
           new Text(
             'Courses in English',
-            style: new TextStyle(fontSize: 30.0, color: Colors.red),
+            style: new TextStyle(fontSize: 30.0, color: CiEColor.red),
           ),
           new Container(
             padding: const EdgeInsets.all(20.0),
             alignment: Alignment.center,
             child: new FlatButton(
               onPressed: () => Navigator.pushReplacementNamed(context, Routes.Login),
-              color: Colors.red,
+              color: CiEColor.red,
+              shape: new RoundedRectangleBorder(borderRadius: CiEStyle.getButtonBorderRadius()),
               padding: new EdgeInsets.all(10.0),
               child: new Text(
                 "Start",
