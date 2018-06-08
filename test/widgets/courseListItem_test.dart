fc3b295827ea8c9b1a3d299481bf04a81c5631be
EduardBartolovic <zxeduardxz@gmail.com>
2018-06-08 16:47:18 +0200

    code clean up
    timeouts

--- test/widgets/courseListItem_test.dart
+++ test/widgets/courseListItem_test.dart
@@ -4,31 +4,42 @@ import 'package:cie_team1/widgets/courseListItem.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 
-
+@Timeout(const Duration(seconds: 5))
 void main() {
 
-  testWidgets('1 courseListPresenterTest', (WidgetTester tester) async {
-    final CourseListPresenter courseListPresenter = new CourseListPresenter();
-    final int id = 1;
 
-    final CourseListItem courseListItem = new CourseListItem(
-        courseListPresenter, id, new IconButton( icon: GenericIcon.buildGenericFavoriteIcon(courseListPresenter.getFavourite(id))));
+  group('simple tests', ()
+  {
 
 
-    expect(courseListItem.courseListPresenter, courseListPresenter);
+    test('1 courseListPresenterTest', () async {
+      final CourseListPresenter courseListPresenter = new CourseListPresenter();
+      final int id = 1;
 
-  });
-
-  testWidgets('1 idTest', (WidgetTester tester) async {
-    final CourseListPresenter courseListPresenter = new CourseListPresenter();
-    final int id = 1;
-
-    final CourseListItem courseListItem = new CourseListItem(
-        courseListPresenter, id, new IconButton( icon: GenericIcon.buildGenericFavoriteIcon(courseListPresenter.getFavourite(id))));
+      final CourseListItem courseListItem = new CourseListItem(
+          courseListPresenter, id, new IconButton(
+          icon: GenericIcon.buildGenericFavoriteIcon(
+              courseListPresenter.getFavourite(id))));
 
 
-    expect(courseListItem.id, 1);
+      expect(courseListItem.courseListPresenter, courseListPresenter);
+    });
 
-  });
+    test('1 idTest', () async {
+      final CourseListPresenter courseListPresenter = new CourseListPresenter();
+      final int id = 1;
 
-}
+      final CourseListItem courseListItem = new CourseListItem(
+          courseListPresenter, id, new IconButton(
+          icon: GenericIcon.buildGenericFavoriteIcon(
+              courseListPresenter.getFavourite(id))));
+
+
+      expect(courseListItem.id, 1);
+    });
+
+
+
+  });//group
+
+}//main
