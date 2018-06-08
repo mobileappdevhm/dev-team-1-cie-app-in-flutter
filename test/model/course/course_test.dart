f9512be7c138d8b2519f5b9d64c15f6ca3cdc592
Eduard <zxeduardxz@gmail.com>
2018-06-09 00:54:46 +0200

    adding test to improve test covergae to 100 %

--- test/model/course/course_test.dart
+++ test/model/course/course_test.dart
@@ -212,6 +212,44 @@ void main() {
 
       expect(sut.available, CourseAvailability.AVAILABLE);
     });
+
+    test('12 ocure', () {
+      final Course sut = new Course(
+        "Blaba",
+        "7",
+        [new Lecture(Campus.KARLSTRASSE, Weekday.Mon, new DayTime(10, 00),
+            new DayTime(11, 30), "R0.009")
+        ],
+        "boring",
+        2,
+        2,
+        "example@hm.edu",
+        "Max Mustermann",
+        CourseAvailability.AVAILABLE,
+        false,
+      );
+
+      expect(sut.occursOnDay(Weekday.Mon), true);
+    });
+
+    test('13 ocure', () {
+      final Course sut = new Course(
+        "Blaba",
+        "7",
+        [new Lecture(Campus.KARLSTRASSE, Weekday.Mon, new DayTime(10, 00),
+            new DayTime(11, 30), "R0.009")
+        ],
+        "boring",
+        2,
+        2,
+        "example@hm.edu",
+        "Max Mustermann",
+        CourseAvailability.AVAILABLE,
+        false,
+      );
+
+      expect(sut.occursOnDay(Weekday.Sun), false);
+    });
   });
 
 
@@ -227,6 +265,43 @@ void main() {
     test('available', () {
       expect(CourseAvailabilityUtility.getFacultyAsInt(CourseAvailability.UNAVAILABLE), 2);
     });
+  });
+
+  group('weekday utility', () {
+    test('1', () {
+      expect(WeekdayUtility.getWeekdayAsString(Weekday.Sun), "Sun");
+    });
+
+    test('2', () {
+      expect(WeekdayUtility.getWeekdayAsString(Weekday.Sat), "Sat");
+    });
+    test('3', () {
+      expect(WeekdayUtility.getWeekdayAsString(Weekday.Fri), "Fri");
+    });
+
+    test('4', () {
+      expect(WeekdayUtility.getWeekdayAsInt(Weekday.Sun), 6);
+    });
+
+    test('5', () {
+      expect(WeekdayUtility.getWeekdayAsInt(Weekday.Sat), 5);
+    });
+    test('6', () {
+      expect(WeekdayUtility.getWeekdayAsInt(Weekday.Fri), 4);
+    });
+
+    test('7', () {
+      expect(WeekdayUtility.intToWeekday(4), Weekday.Fri);
+    });
+
+    test('8', () {
+      expect(WeekdayUtility.intToWeekday(5), Weekday.Sat);
+    });
+    test('9', () {
+      expect(WeekdayUtility.intToWeekday(6), Weekday.Sun);
+    });
+
+    //could do more.... but others are already covered
   });
 }
 
