d26333c5c8198e0a2cea326feb759e676c7a1054
Tobias Redl <huabatobs@gmail.com>
2018-06-09 07:31:07 +0200

    undo changes on tabs, bottom navigation bar has some trouble with localization

--- lib/views/tabs.dart
+++ lib/views/tabs.dart
@@ -18,20 +18,14 @@ class TabsPageState extends State<TabsPage> {
   PageController _tabController;
   CourseListPresenter courseListPresenter;
   var _appTitle = '';
-  var _tabItems = new List<TabItem>();
+
+  //var _tabItems = new List<TabItem>(); comment is used to solve the string problem from the bottom
 
   int _tab = 2; //change this to the default tab page value
 
   @override
   void initState() {
     super.initState();
-
-    _tabItems.add(new TabItem(S.of(context).tabs_courses, Icons.import_contacts));
-    _tabItems.add(new TabItem(S.of(context).tabs_maps, Icons.map));
-    _tabItems.add(new TabItem(S.of(context).tabs_schedule, Icons.event_note));
-    _tabItems.add(new TabItem(S.of(context).tabs_favorites, Icons.favorite_border));
-    _tabItems.add(new TabItem(S.of(context).tabs_settings, Icons.settings));
-
     _tabController = new PageController(initialPage: _tab);
     courseListPresenter = new CourseListPresenter();
     this._appTitle = _tabItems[_tab].title;
@@ -45,6 +39,12 @@ class TabsPageState extends State<TabsPage> {
 
   @override
   Widget build(BuildContext context) {
+    //comment is used to solve the string problem from the bottom
+    //_tabItems.add(new TabItem(S.of(context).tabs_courses, Icons.import_contacts));
+    //_tabItems.add(new TabItem(S.of(context).tabs_maps, Icons.map));
+    //_tabItems.add(new TabItem(S.of(context).tabs_schedule, Icons.event_note));
+    //_tabItems.add(new TabItem(S.of(context).tabs_favorites, Icons.favorite_border));
+    //_tabItems.add(new TabItem(S.of(context).tabs_settings, Icons.settings));
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(
@@ -95,9 +95,26 @@ class TabsPageState extends State<TabsPage> {
   }
 }
 
-class TabItem {
-  String title;
-  IconData icon;
+//comment is used to solve the string problem from below
+//class TabItem {
+//  String title;
+//  IconData icon;
+//
+//  TabItem(this.title, this.icon);
+//}
 
-  TabItem(this.title, this.icon);
+class TabItem {
+  const TabItem({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
 }
+
+//TODO this string couldn't replaced yet, because there throwing errors at build state - fix will foloow
+const List<TabItem> _tabItems = const <TabItem>[
+  const TabItem(title: 'Courses', icon: Icons.import_contacts),
+  const TabItem(title: 'Map', icon: Icons.map),
+  const TabItem(title: 'Timetable', icon: Icons.event_note),
+  const TabItem(title: 'Favorites', icon: Icons.favorite_border),
+  const TabItem(title: 'Settings', icon: Icons.settings),
+];
