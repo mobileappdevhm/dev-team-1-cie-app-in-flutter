0b0327796e1b3e1dbd87c98c84625a7eba4d9f48
Eduard <zxeduardxz@gmail.com>
2018-06-09 10:19:13 +0200

    tests will work now

--- test/views/tab_test.dart
+++ test/views/tab_test.dart
@@ -5,10 +5,7 @@ import 'package:flutter/material.dart';
 
 @Timeout(const Duration(seconds: 20))
 void main() {
-
-
-  group('settingspagewidget', ()
-  {
+  group('settingspagewidget', () {
     testWidgets('1 widgetTest for settings', (WidgetTester tester) async {
 // Tells the tester to build a UI based on the widget tree passed to it
       await tester.pumpWidget(
@@ -16,9 +13,7 @@ void main() {
           builder: (BuildContext context, StateSetter setState) {
             return new MaterialApp(
               home: new Material(
-                child: new Center(
-                    child: new TabsPage()
-                ),
+                child: new Center(child: new TabsPage()),
               ),
             );
           },
@@ -30,36 +25,44 @@ void main() {
       int counter = 0;
       for (Widget widget in listOfWidgets) {
         if (widget is Text) {
-          if (counter == 0) {
-            expect(widget.data, 'Today');
-            counter++;
-          } else if (counter == 1) {
-            expect(widget.data.length, 3);//I dont know why it doesnt work -_-//WeekdayUtility.getWeekdayAsString(WeekdayUtility.getCurrentWeekday()));
-            counter++;
-          } else if (counter == 2) {
-            expect(widget.data.substring(0,15), "Title of Course");
-            counter++;
-          }else if (counter == 3) {
-            expect(widget.data.substring(0,13), ' - Professor ');
-            counter++;
-          }else if (counter == 4) {
-            expect(widget.data.substring(0,8), 'Campus: ');
-            counter++;
-          } else if (counter == 5) {
-            expect(widget.data, 'Pasing');
-            counter++;
+          if (WeekdayUtility.getCurrentWeekday() == Weekday.Sun ||
+              WeekdayUtility.getCurrentWeekday() == Weekday.Sat) { // on weekend arent any courses
+            if (counter == 0) {
+              expect(widget.data, 'Today');
+              counter++;
+            } else if (counter == 1) {
+              expect(
+                  widget.data,
+                  WeekdayUtility
+                      .getWeekdayAsString(WeekdayUtility.getCurrentWeekday()));
+              counter++;
+            }
+          } else {
+            if (counter == 0) {
+              expect(widget.data, 'Today');
+              counter++;
+            } else if (counter == 1) {
+              expect(
+                  widget.data,
+                  WeekdayUtility
+                      .getWeekdayAsString(WeekdayUtility.getCurrentWeekday()));
+              counter++;
+            } else if (counter == 2) {
+              expect(widget.data.substring(0, 15), "Title of Course");
+              counter++;
+            } else if (counter == 3) {
+              expect(widget.data.substring(0, 13), ' - Professor ');
+              counter++;
+            } else if (counter == 4) {
+              expect(widget.data.substring(0, 8), 'Campus: ');
+              counter++;
+            } else if (counter == 5) {
+              expect(widget.data, 'Pasing');
+              counter++;
+            }
           }
-
         }
       }
     });
-
-
-
-
-
   });
-
-
-
 }
