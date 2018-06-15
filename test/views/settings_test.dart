00dbc1e23d02cccee9c4b41c417f44d20a01e265
EduardBartolovic <zxeduardxz@gmail.com>
2018-06-15 10:18:12 +0200

    tests for settings

--- test/views/settings_test.dart
+++ test/views/settings_test.dart
@@ -8,7 +8,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('settingspagewidget', () {
     testWidgets('1 widgetTest for settings', (WidgetTester tester) async {
-// Tells the tester to build a UI based on the widget tree passed to it
+    // Tells the tester to build a UI based on the widget tree passed to it
       await tester.pumpWidget(
         new StatefulBuilder(
           builder: (BuildContext context, StateSetter setState) {
@@ -69,6 +69,44 @@ void main() {
             counter++;
           } else if (counter == 13) {
             expect(widget.data, StaticVariables.TAKEN_COURSES);
+            counter++;
+          }
+        }
+      }
+    });
+
+
+    testWidgets('2 widgetTest for settings', (WidgetTester tester) async {
+      // Tells the tester to build a UI based on the widget tree passed to it
+      await tester.pumpWidget(
+        new StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return new MaterialApp(
+              home: new Material(
+                child: new Center(child: new Settings()),
+              ),
+            );
+          },
+        ),
+      );
+
+      final Iterable<Widget> listOfWidgets = tester.allWidgets;
+
+      int counter = 0;
+      for (Widget widget in listOfWidgets) {
+        if (widget is Text) {
+          if (counter == 0) {
+            expect(widget.data, StaticVariables.LOGGED_IN_AS);
+            counter++;
+          } else if (counter == 1) {
+            expect(widget.data, " " + new CurrentUserPresenter().getFullName());
+            counter++;
+          } else if (counter == 2) {
+            expect(widget.data, StaticVariables.LOGOUT_BUTTON);
+            counter++;
+          } else if (counter == 3) {
+            expect(widget.data, StaticVariables.PRIVACY_BUTTON);
+            tester.tap(find.byWidget(widget));
             counter++;
           }
         }
