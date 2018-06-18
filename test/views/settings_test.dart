1e8f762618065686e8257c4572170700c163c0ae
Eduard <zxeduardxz@gmail.com>
2018-06-18 20:09:28 +0200

    lets go travis

--- test/views/settings_test.dart
+++ test/views/settings_test.dart
@@ -9,12 +9,13 @@ void main() {
   group('settingspagewidget', () {
     testWidgets('1 widgetTest for settings', (WidgetTester tester) async {
     // Tells the tester to build a UI based on the widget tree passed to it
+      ValueChanged<bool> boolean ;
       await tester.pumpWidget(
         new StatefulBuilder(
           builder: (BuildContext context, StateSetter setState) {
             return new MaterialApp(
               home: new Material(
-                child: new Center(child: new Settings()),
+                child: new Center(child: new Settings(new CurrentUserPresenter(boolean, Flavor.PROD))),
               ),
             );
           },
@@ -30,10 +31,10 @@ void main() {
             expect(widget.data, StaticVariables.LOGGED_IN_AS);
             counter++;
           } else if (counter == 1) {
-            expect(widget.data, " " + new CurrentUserPresenter().getFullName());
+            expect(widget.data, " " + new CurrentUserPresenter(boolean, Flavor.PROD).getFullName());
             counter++;
           } else if (counter == 2) {
-            expect(widget.data, StaticVariables.LOGOUT_BUTTON);
+            expect(widget.data, StaticVariables.LOGIN_BUTTON);
             counter++;
           } else if (counter == 3) {
             expect(widget.data, StaticVariables.PRIVACY_BUTTON);
@@ -43,14 +44,14 @@ void main() {
             counter++;
           } else if (counter == 5) {
             expect(widget.data,
-                " " + new CurrentUserPresenter().getCurrentUserStatus());
+                " " + new CurrentUserPresenter(boolean, Flavor.PROD).getCurrentUserStatus());
             counter++;
           } else if (counter == 6) {
             expect(widget.data, StaticVariables.DEPARTMENT + " : ");
             counter++;
           } else if (counter == 7) {
             expect(widget.data,
-                " " + new CurrentUserPresenter().getCurrentUserFaculty());
+                " " + new CurrentUserPresenter(boolean, Flavor.PROD).getCurrentUserFaculty());
             counter++;
           } else if (counter == 8) {
             expect(widget.data, StaticVariables.CONTACT_OFFICE);
@@ -78,12 +79,13 @@ void main() {
 
     testWidgets('2 widgetTest for settings', (WidgetTester tester) async {
       // Tells the tester to build a UI based on the widget tree passed to it
+      ValueChanged<bool> boolean;
       await tester.pumpWidget(
         new StatefulBuilder(
           builder: (BuildContext context, StateSetter setState) {
             return new MaterialApp(
               home: new Material(
-                child: new Center(child: new Settings()),
+                child: new Center(child: new Settings(new CurrentUserPresenter(boolean, Flavor.PROD))),
               ),
             );
           },
@@ -99,10 +101,10 @@ void main() {
             expect(widget.data, StaticVariables.LOGGED_IN_AS);
             counter++;
           } else if (counter == 1) {
-            expect(widget.data, " " + new CurrentUserPresenter().getFullName());
+            expect(widget.data, " " + new CurrentUserPresenter(boolean, Flavor.MOCK).getFullName());
             counter++;
           } else if (counter == 2) {
-            expect(widget.data, StaticVariables.LOGOUT_BUTTON);
+            expect(widget.data, StaticVariables.LOGIN_BUTTON);
             counter++;
           } else if (counter == 3) {
             expect(widget.data, StaticVariables.PRIVACY_BUTTON);
@@ -114,13 +116,15 @@ void main() {
     });
 
     testWidgets('3 widgetTest for settings Pressing Taken Courses Button', (WidgetTester tester) async {
+
+      ValueChanged<bool> boolean;
       // Tells the tester to build a UI based on the widget tree passed to it
       await tester.pumpWidget(
         new StatefulBuilder(
           builder: (BuildContext context, StateSetter setState) {
             return new MaterialApp(
               home: new Material(
-                child: new Center(child: new Settings()),
+                child: new Center(child: new Settings(new CurrentUserPresenter(boolean, Flavor.PROD))),
               ),
             );
           },
@@ -136,10 +140,10 @@ void main() {
             expect(widget.data, StaticVariables.LOGGED_IN_AS);
             counter++;
           } else if (counter == 1) {
-            expect(widget.data, " " + new CurrentUserPresenter().getFullName());
+            expect(widget.data, " " + new CurrentUserPresenter(boolean, Flavor.PROD).getFullName());
             counter++;
           } else if (counter == 2) {
-            expect(widget.data, StaticVariables.LOGOUT_BUTTON);
+            expect(widget.data, StaticVariables.LOGIN_BUTTON);
             counter++;
           } else if (counter == 3) {
             expect(widget.data, StaticVariables.PRIVACY_BUTTON);
@@ -149,13 +153,13 @@ void main() {
             counter++;
           } else if (counter == 5) {
             expect(widget.data,
-                " " + new CurrentUserPresenter().getCurrentUserStatus());
+                " " + new CurrentUserPresenter(boolean, Flavor.PROD).getCurrentUserStatus());
             counter++;
           } else if (counter == 6) {
             expect(widget.data, StaticVariables.DEPARTMENT + " : ");
             counter++;
           } else if (counter == 7) {
-            expect(widget.data, " " + new CurrentUserPresenter().getCurrentUserFaculty());
+            expect(widget.data, " " + new CurrentUserPresenter(boolean, Flavor.PROD).getCurrentUserFaculty());
             counter++;
           } else if (counter == 8) {
             expect(widget.data, StaticVariables.CONTACT_OFFICE);
