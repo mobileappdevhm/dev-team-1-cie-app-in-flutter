a669f2841068f49b0f114b9fbd6de4d81a642758
Sean Wallace <sean@Seans-MacBook-Pro.local>
2018-06-19 17:11:54 +0200

    themeModifications to tab names & an icon

--- lib/views/tabs.dart
+++ lib/views/tabs.dart
@@ -112,7 +112,7 @@ class TabItem {
 const List<TabItem> TabItems = const <TabItem>[
   const TabItem(title: 'Courses', icon: Icons.import_contacts),
   const TabItem(title: 'Map', icon: Icons.map),
-  const TabItem(title: 'Timetable', icon: Icons.event_note),
+  const TabItem(title: 'Schedule', icon: Icons.event_note),
   const TabItem(title: 'Favorites', icon: Icons.favorite_border),
-  const TabItem(title: 'Settings', icon: Icons.settings),
+  const TabItem(title: 'Profile', icon: Icons.person),
 ];
