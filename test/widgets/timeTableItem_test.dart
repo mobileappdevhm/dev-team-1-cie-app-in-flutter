eae97156b318463f7df3f3e676903d9a1d741cf1
Sean Wallace <sean@Seans-MacBook-Pro.local>
2018-06-19 18:45:03 +0200

    Adding some tests.

---
+++ test/widgets/timeTableItem_test.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:cie_team1/widgets/timeTableItem.dart';
+import 'package:cie_team1/model/course/course.dart';
+
+@Timeout(const Duration(seconds: 20))
+void main() {
+  Lecture testLectureOne;
+  Course testCourseOne;
+
+  setUp((){
+    testLectureOne = new Lecture(Campus.KARLSTRASSE, Weekday.Mon,
+        new DayTime(10, 00), new DayTime(11, 30), "R0.009") ;
+    testCourseOne = new CourseBuilder()
+      .withName("Blaba")
+      .withFaculty("7")
+      .withLecturesPerWeek([
+        testLectureOne
+      ])
+      .withDescription("boring")
+      .withHoursPerWeek(2)
+      .withEcts(2)
+      .withProfessorEmail("example@hm.edu")
+      .withProfessorName("Max Mustermann")
+      .withAvailable(CourseAvailability.AVAILABLE)
+      .withIsFavorite(false)
+      .build();
+    testLectureOne.course = testCourseOne;
+  });
+
+  group('settingspagewidget', () {
+    testWidgets('1 widgetTest for settings', (WidgetTester tester) async {
+// Tells the tester to build a UI based on the widget tree passed to it
+      await tester.pumpWidget(
+        new StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return new MaterialApp(
+              home: new Material(
+                child: new Center(child: new TimeTableItem(testLectureOne)),
+              ),
+            );
+          },
+        ),
+      );
+
+      final Iterable<Widget> listOfWidgets = tester.allWidgets;
+      int i = 0;
+      List<String> expectedTextWidgetData = [
+      'Blaba',
+      'Campus:',
+      'Karl.',
+      'Time:',
+      ' 10:00-11:30',
+      'Day:',
+      'Mon',
+      'Room:',
+      ' R0.009',
+      'Professor:',
+      'Max Mustermann'
+      ];
+      for (Widget widget in listOfWidgets) {
+        if (widget is Text) {
+          expect(widget.data.contains(expectedTextWidgetData.elementAt(i)), true);
+          i++;
+        }
+      }
+    });
+  });
+}
