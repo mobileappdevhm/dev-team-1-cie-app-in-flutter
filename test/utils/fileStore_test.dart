a2425cf179de6a66a1e11137d17620cdfc70f312
EduardBartolovic <zxeduardxz@gmail.com>
2018-06-21 09:22:58 +0200

    FileStore test

---
+++ test/utils/fileStore_test.dart
@@ -0,0 +1,17 @@
+import 'package:cie_team1/utils/fileStore.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+@Timeout(const Duration(seconds: 40))
+void main() {
+  test('1 widgetTest', (){
+
+    FileStore.writeToFile("test","data");
+
+    FileStore.getFile("test");
+
+    expect(FileStore.readFileAsString("test").runtimeType, new Future.value("data").runtimeType);
+
+  });
+
+
+}
