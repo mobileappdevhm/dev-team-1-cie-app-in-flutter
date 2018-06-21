d456af9c6112c0988a0a75178ed1b301515c05ce
Tobias Redl <huabatobs@gmail.com>
2018-06-21 09:34:49 +0200

    added nine course structure

---
+++ lib/model/course/details/lecturer.dart
@@ -0,0 +1,25 @@
+class LecturerBuilder {
+  static List<Lecturer> fromJson(List<dynamic> jsonData){
+    if(jsonData == null)
+      return null;
+    var list = new List<Lecturer>();
+    for(int i = 0; i < jsonData.length; i++) {
+      list.add(new Lecturer(jsonData[i]));
+    }
+    return list;
+  }
+}
+
+class Lecturer {
+  String title;
+  String firstName;
+  String lastName;
+  List<dynamic> actions;
+
+  Lecturer(Map<String, dynamic> jsonData){
+    this.title = jsonData['title'];
+    this.firstName = jsonData['firstName'];
+    this.lastName = jsonData['lastName'];
+    this.actions = jsonData['actions'];
+  }
+}
