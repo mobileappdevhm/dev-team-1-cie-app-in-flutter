96386914d3aaf4a631c1783b4cdce70fb84fbc52
Sean Wallace <sean@dhcp-10-179-11-112.dynamic.eduroam.mwn.de>
2018-06-22 09:05:07 +0200

    add nine test

--- test/utils/nineAPIConsumer_test.dart
+++ test/utils/nineAPIConsumer_test.dart
@@ -1,12 +1,16 @@
 import 'package:cie_team1/utils/nineAPIConsumer.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:connectivity/connectivity.dart';
 
 @Timeout(const Duration(seconds: 40))
 void main() {
-  //TODO tests without expect sometimes make trouble on travis
-  //as a hint, I think because this function is a async one, maybe expectAsync is your friend (but I don't know :-) )
-  //test('1 pullCourseJson', (){
-  //  NineAPIEngine.pullCourseJSON(null, true);
-  //  //maybe adding Fileread
-  //});
+  test('test Internet connectivity', () {
+    NineAPIEngine.isInternetConnected().then((isConnected) {
+      (new Connectivity().checkConnectivity()).then((connectivityResult) {
+        bool testLogic = (connectivityResult == ConnectivityResult.mobile ||
+            connectivityResult == ConnectivityResult.wifi) ? true: false;
+        expect(isConnected, testLogic);
+      });
+    });
+  });
 }
