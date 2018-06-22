9fefb6db62f7eca38b32731b2367f62497be592d
Tobias Redl <huabatobs@gmail.com>
2018-06-22 03:54:27 +0200

    roombuilder test

---
+++ test/model/course/details/room_test.dart
@@ -0,0 +1,23 @@
+import 'package:cie_team1/model/course/details/room.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  test('1 room',(){
+    String campus = "Lothstrasse";
+    String building = "R";
+    String number = "R 2.091";
+
+    var room = new Map<String, dynamic>();
+    room['number'] = number;
+    room['building'] = building;
+    room['campus'] = campus;
+    var rooms = new List<Map<String, dynamic>>();
+    rooms.add(room);
+
+    var list = RoomBuilder.fromJson(rooms);
+
+    expect(list[0].campus, campus);
+    expect(list[0].building, building);
+    expect(list[0].number, number);
+  });
+}
