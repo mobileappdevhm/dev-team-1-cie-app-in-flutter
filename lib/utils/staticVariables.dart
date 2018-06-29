867efe6bd1e309702cc081d96bcbc8984537f255
Tobias Redl <huabatobs@gmail.com>
2018-06-29 10:12:27 +0200

    text fix

--- lib/utils/staticVariables.dart
+++ lib/utils/staticVariables.dart
@@ -119,7 +119,7 @@ class StaticVariables {
   static const String INSTRUCTIONS_BUTTON_TEXT =
       "Download courses now and restart";
   static const String INSTRUCTIONS_PLEASE_GO_TO =
-      "Plase go to courses page and refresh by swiping down.";
+      "Please go to courses page and refresh by swiping down.";
 
   static const String PRIVACY_TEXT_GERMAN = "Diese Datenschutzerklärung klärt Sie über die Art, den Umfang und Zweck der Verarbeitung von personenbezogenen Daten (nachfolgend kurz “Daten”) innerhalb unseres Onlineangebotes und der mit ihm verbundenen Webseiten, Funktionen und Inhalte sowie externen Onlinepräsenzen, wie z.B. unser Social Media Profile auf (nachfolgend gemeinsam bezeichnet als “Onlineangebot”). Im Hinblick auf die verwendeten Begrifflichkeiten, wie z.B. “Verarbeitung” oder “Verantwortlicher” verweisen wir auf die Definitionen im Art. 4 der Datenschutzgrundverordnung (DSGVO)\n\n" +
       "Arten der verarbeiteten Daten:\n" +
