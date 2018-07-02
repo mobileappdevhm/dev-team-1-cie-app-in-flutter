84925fa33074f572b7afa728246905100be120fe
Eduard <zxeduardxz@gmail.com>
2018-07-02 20:27:21 +0200

    tests for courseList logged in

--- test/widgets/courseList_test.dart
+++ test/widgets/courseList_test.dart
@@ -24,7 +24,7 @@ void main() {
             .add("Department #" + CoursesMock.generateMockDepartment(i));
         expectedTexts.add(CoursesMock.generateMockCourseTitle(i));
         expectedTexts.add("DP " + CoursesMock.generateMockDepartment(i));
-        expectedTexts.add("Please log in to use lottery");
+        expectedTexts.add(StaticVariables.FAVORITES_REGISTRATION_BUTTON_LOGIN_FIRST);
         //expectedTexts.add("Time: " + CoursesMock.generateMockTime(i));
       }
       favoritesPageAnomalies.add(StaticVariables.FAVORITES_REGISTRATION_BUTTON);
@@ -81,36 +81,64 @@ void main() {
       checkForFavoritesPageAnomalies(
           listOfWidgets, favoritesPageAnomalies, isFavoritesPage);
     });
-
-  testWidgets('pressing submit on favorite page', (WidgetTester tester) async {
-    final bool isFavoritesPage = true;
-    // This widget tree builds the widgets found on the Courses Page
-    await tester.pumpWidget(
-      new StatefulBuilder(
-        builder: (BuildContext context, StateSetter setState) {
-          return new MaterialApp(
-            home: new Material(
-              child: new Center(
-                  child: new CourseList(clp, isFavoritesPage, cup, fN)),
-            ),
-          );
-        },
-      ),
-    );
-    final Iterable<Widget> listOfWidgets = tester.allWidgets;
-
-    int counter = 0;
-    for (Widget widget in listOfWidgets) {
-      if (widget is Text) {
-        if (counter == 0) {
-          expect(widget.data,StaticVariables.FAVORITES_REGISTRATION_BUTTON_LOGIN_FIRST);
-        }
-        counter++;
-      }
-    }
-  });
-
 });
+
+  group("2 user is logged in", () {
+    // Expected widget within created widgets. Generated using our mock's behavior
+    List<String> expectedTexts;
+    // Texts that are unique to the favorites page
+    List<String> favoritesPageAnomalies;
+    CourseListPresenter clp;
+    CurrentUserPresenter cup;
+    FocusNode fN;
+
+    setUp(() {
+      expectedTexts = new List<String>();
+      favoritesPageAnomalies = new List<String>();
+      for (int i = 1; i < 100; i++) {
+        expectedTexts
+            .add("Department #" + CoursesMock.generateMockDepartment(i));
+        expectedTexts.add(CoursesMock.generateMockCourseTitle(i));
+        expectedTexts.add("DP " + CoursesMock.generateMockDepartment(i));
+        expectedTexts.add(StaticVariables.FAVORITES_REGISTRATION_BUTTON);
+      }
+      favoritesPageAnomalies.add(StaticVariables.FAVORITES_REGISTRATION_BUTTON);
+
+      clp = new CourseListPresenter(_voidCallback, Flavor.MOCK);
+      cup = new CurrentUserPresenter(_voidCallback, Flavor.MOCK);
+      cup.getCurrentUser().isLoggedIn = true;
+      fN = new FocusNode();
+    });
+
+    testWidgets('pressing submit on favorite page', (WidgetTester tester) async {
+      final bool isFavoritesPage = true;
+      // This widget tree builds the widgets found on the Courses Page
+      await tester.pumpWidget(
+        new StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return new MaterialApp(
+              home: new Material(
+                child: new Center(
+                    child: new CourseList(clp, isFavoritesPage, cup, fN)),
+              ),
+            );
+          },
+        ),
+      );
+      final Iterable<Widget> listOfWidgets = tester.allWidgets;
+
+      int counter = 0;
+      for (Widget widget in listOfWidgets) {
+        if (widget is Text) {
+          if (counter == 0) {
+            expect(widget.data,StaticVariables.FAVORITES_REGISTRATION_BUTTON);
+          }
+          counter++;
+        }
+      }
+    });
+
+  });
 }
 
 void checkForFavoritesPageAnomalies(Iterable<Widget> listOfWidgets,
