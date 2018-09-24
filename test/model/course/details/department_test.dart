68c899f6c8a8a3956a6ad56dcb399b76fe0b005a
Tobias Redl <huabatobs@gmail.com>
2018-09-24 16:44:07 +0200

    added more tests

--- test/model/course/details/department_test.dart
+++ test/model/course/details/department_test.dart
@@ -2,7 +2,15 @@ import 'package:cie_app/model/course/details/department.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('1', () {
+
+  final dynamic jsonData = {
+  "color": "#008E7D",
+  "name": "Fakultät 07",
+  "shortName": "FK 07",
+  "actions": []
+  };
+
+  test('department list', () {
     int counter = 1;
     for (String sut in Department.departments.toList()) {
       if (counter < 10)
@@ -11,6 +19,17 @@ void main() {
         expect(sut, counter.toString());
       counter++;
     }
+  });
+
+  test('departments fromJson', () {
+    var departmentList = List<dynamic>();
+    departmentList.add(jsonData);
+
+    var department = Department.fromJson(departmentList)[0];
+
+    expect(department.color, "#008E7D");
+    expect(department.name, "Fakultät 07");
+    expect(department.shortName, "FK 07");
   });
 
 
