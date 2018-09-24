de6fa6ce63555d82dd28f0f75f585c0b3b9de9cc
Tobias Redl <huabatobs@gmail.com>
2018-09-24 13:31:17 +0200

    - remove office specific html tags (<o:?></o:?) from course description - remove empty tags, or empty whitespace tags such as <p></p> etc from course description

--- lib/model/course/course.dart
+++ lib/model/course/course.dart
@@ -31,8 +31,14 @@ class Course {
 
   Course(Map<String, dynamic> jsonData) {
     id = jsonData['id'];
-    description =
-        jsonData['description'] != null ? jsonData['description'] : "";
+    description = jsonData['description'] != null
+        ? jsonData['description']
+            .replaceAll('<o:', '<')
+            .replaceAll('</o:', '</') //remove office specific
+    .replaceAllMapped(new RegExp(r'<([^>\s]+)[^>]*>(?:\s*(?:&nbsp;|&thinsp;|&ensp;|&emsp;|&#8201;|&#8194;|&#8195;)\s*)*<\/\1>'), (match){
+      return ''; //remove empty html tags such as <p></p> or some with whitespace <p>&nbsp;</p>
+    })
+        : "";
     isCoterie = jsonData['isCoterie'];
     hasHomeBias = jsonData['hasHomeBias'];
     category = jsonData['category'];
